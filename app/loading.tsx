86f93581844083484feb47555c6269cea1205216
Redot2468 <lawalridwan860@gmail.com>
2025-05-01 11:47:55 +0100

    fix: loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Loader from "@/app/_components/Loader";
 
 function Loading() {
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import ProjectsBlock from "@/app/_components/projects/ProjectsBlock";
 import Story from "@/app/_components/Story";
 
 export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <div className="mx-auto max-w-[1200px]">
       <div>
